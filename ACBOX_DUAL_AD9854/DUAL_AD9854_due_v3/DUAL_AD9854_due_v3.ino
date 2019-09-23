3c28d70b6b083d5e840f27fa5976a13e5f2a5ab1
Carlos Kometter <ckometter@gmail.com>
2019-09-23 15:43:10 -0700

    Fix __throw_bad_alloc() error

--- ACBOX_DUAL_AD9854/DUAL_AD9854_due_v3/DUAL_AD9854_due_v3.ino
+++ ACBOX_DUAL_AD9854/DUAL_AD9854_due_v3/DUAL_AD9854_due_v3.ino
@@ -50,6 +50,19 @@ void setup()
   }
 }
 
+namespace std {
+  void __throw_bad_alloc()
+  {
+    Serial.println("Unable to allocate memory");
+  }
+
+  void __throw_length_error( char const*e )
+  {
+    Serial.print("Length Error :");
+    Serial.println(e);
+  }
+}
+
 void flash(int port, int ms)
 {
   digitalWrite(port,HIGH);
